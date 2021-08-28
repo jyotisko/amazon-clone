f516b77d53a0d0f8418a779c1f4d3e422233ebb4
jyotisko <jyotisko2006@gmail.com>
2021-08-28 23:24:37 +0530

    Implemented webhook

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -14,7 +14,7 @@ const setCookie = (req, res, token) => {
   res.cookie('jwt', token, {
     expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
     httpOnly: true,
-    secure: req.secure || req.headers['x-forwarded-proto'] === 'https'
+    secure: process.env.NODE_ENV === 'development' ? false : true
   });
 };
 
