15089c39966a0b6cbd7381ff0f2e2395355e3e3a
jyotisko <jyotisko2006@gmail.com>
2021-08-28 23:35:13 +0530

    Implemented webhook

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -14,7 +14,8 @@ const setCookie = (req, res, token) => {
   res.cookie('jwt', token, {
     expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
     httpOnly: true,
-    secure: false
+    secure: false,
+    domain: 'https://amazon-clone-jyotisko.vercel.app/'
   });
 };
 
