375617b9e40de2f6823dc588a099d6f4983c2586
jyotisko <jyotisko2006@gmail.com>
2021-08-29 17:39:44 +0530

    Preparing for deployment

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -15,7 +15,7 @@ const setCookie = (req, res, token) => {
     expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
     // httpOnly: false,
     // secure: false,
-    // domain: 'amazon-clone-jyotisko.vercel.app'
+    domain: 'vercel.app'
   });
 };
 
