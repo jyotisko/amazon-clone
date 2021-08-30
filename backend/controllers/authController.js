59631d70447ff5753a7fb944421e2da02465184e
jyotisko <jyotisko2006@gmail.com>
2021-08-30 17:31:56 +0530

    Fixing deployment bug

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -14,9 +14,9 @@ const setCookie = (req, res, token) => {
   res.cookie('jwt', token, {
     expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
     // httpOnly: false,
-    // secure: false,
+    secure: true,
     // domain: 'vercel.app',
-    // sameSite: 'none'
+    sameSite: 'none'
   });
 };
 
