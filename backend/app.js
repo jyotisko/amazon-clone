679c5573408c283db308219446f5400cb01a4728
jyotisko <jyotisko2006@gmail.com>
2021-08-30 19:43:04 +0530

    Fixing deployment bug

--- backend/app.js
+++ backend/app.js
@@ -41,13 +41,6 @@ app.post('/webhook-checkout', bodyParser.raw({ type: 'application/json' }), purc
 app.use(express.json());
 
 // ROUTES
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Credentials', true);
-//   res.header('Access-Control-Allow-Origin', 'https://amazon-clone-jyotisko.vercel.app');
-//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
-//   res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
-//   next();
-// });
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/products', productRouter);
 app.use('/api/v1/reviews', reviewRouter);
