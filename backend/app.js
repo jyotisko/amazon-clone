fd00c627a5299c9e5ed30b65a763acfef65d737a
jyotisko <jyotisko2006@gmail.com>
2021-08-30 16:30:02 +0530

    Fixing deployment bug

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,18 @@ const AppError = require('./utils/AppError');
 const app = express();
 
 // MIDDLEWARES
-app.use(cors({ credentials: true }));
+const whitelist = ['http://localhost:3000', 'https://amazon-clone-jyotisko.vercel.app/', 'https://amazon-clone-api.vercel.app/'];
+const corsOptions = {
+  credentials: true,
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) callback(null, true)
+    else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+app.use(cors(corsOptions));
 app.use(helmet());
 app.use(cookieParser());
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
