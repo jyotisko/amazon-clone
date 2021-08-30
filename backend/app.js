c4471bbc3882bbadab819f26538f0f009384020e
jyotisko <jyotisko2006@gmail.com>
2021-08-30 17:45:26 +0530

    Fixing deployment bug

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,7 @@ const AppError = require('./utils/AppError');
 const app = express();
 
 // MIDDLEWARES
-const whitelist = ['http://localhost:3000', 'http://localhost:5000', 'https://amazon-clone-jyotisko.vercel.app', 'https://amazon-clone-api.vercel.app'];
+const whitelist = ['http://localhost:3000', 'https://amazon-clone-jyotisko.vercel.app', 'https://amazon-clone-api.vercel.app'];
 const corsOptions = {
   credentials: true,
   origin: function (origin, callback) {
@@ -31,6 +31,13 @@ const corsOptions = {
 }
 
 app.use(cors(corsOptions));
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Origin', 'https://amazon-clone-jyotisko.vercel.app');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+  next();
+});
 app.use(helmet());
 app.use(cookieParser());
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
