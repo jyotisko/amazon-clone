780662a6a57eddb0b2fb2c7aa71c35817ca8a8c1
jyotisko <jyotisko2006@gmail.com>
2021-08-31 17:54:07 +0530

    Setting up webhooks

--- backend/utils/cors.js
+++ backend/utils/cors.js
@@ -1,10 +1,4 @@
 const whitelist = [
-  'http://localhost:3000',
-  'http://localhost:5000',
-  'http://127.0.0.1:3000',
-  'http://127.0.0.1:5000',
-  'https://amazon-clone-jyotisko.vercel.app',
-  'https://amazon-clone-api.vercel.app',
   'http://localhost:3000/',
   'http://localhost:5000/',
   'http://127.0.0.1:3000/',
@@ -12,12 +6,11 @@ const whitelist = [
   'https://amazon-clone-jyotisko.vercel.app/',
   'https://amazon-clone-api.vercel.app/'
 ];
+
 exports.corsOptions = {
   credentials: true,
   origin: function (origin, callback) {
-    console.log(origin)
-    if (whitelist.indexOf(origin) !== -1) return callback(null, true);
-    else return callback(new Error('Not allowed by CORS'));
+    return callback(null, true);
   }
 };
 
