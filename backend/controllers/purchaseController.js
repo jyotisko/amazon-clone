a7c98d025ba0fb43dd76c2049c89e17a12b72aaf
jyotisko <jyotisko2006@gmail.com>
2021-08-31 18:57:30 +0530

    Setting up webhooks

--- backend/controllers/purchaseController.js
+++ backend/controllers/purchaseController.js
@@ -19,7 +19,9 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   const url = process.env.NODE_ENV === 'production' ? 'https://amazon-clone-jyotisko.vercel.app' : 'http://localhost:3000';
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
-    metadata: items,
+    metadata: {
+      products: items
+    },
     line_items: items,
     customer_email: req.user.email,
     client_reference_id: (req.body.products.map((product) => product.productId)).join('&'),
